4b7447a1894e7afcf389d0914055df49b59e5fed
Szymon Markiewicz <szymon.markiewicz.luna@gmail.com>
2025-03-06 17:22:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace pp_lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fizzBuzzLength = 100;
+            
+            for (int i = 0; i < fizzBuzzLength; i++)
+            {
+                string line = "";
+
+                if (i % 3 == 0)
+                {
+                    line += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    line += "Buzz";
+                }
+
+                if (string.IsNullOrEmpty(line))
+                {
+                    line = i.ToString();
+                }
+
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
